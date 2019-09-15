589b509bbbb3a3da165fa989990ce519cf50de05
Mark Kauffman <markkauffman22@gmail.com>
2019-09-15 16:23:03 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,6 +1,5 @@
 var index = require("./index.js");
+var height = require("./index.js");
 
 console.log("WE HAVE " + index.name);
-
-var height = require("./index.js");
 console.log("Height is " + index.height);
